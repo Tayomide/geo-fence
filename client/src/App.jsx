7bac261f395e6d690683374dcb049e7d9195d736
Tayomide <70061548+Tayomide@users.noreply.github.com>
2023-10-20 23:36:58 -0500

    Initialize global variables and default logic

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,67 +1,122 @@
 import styled from "styled-components"
 import { useEffect, useState } from "react"
-import SVG from "./components/SVG"
+import {Routes, Route } from "react-router-dom"
 import AppContext from "./hooks/AppContext"
-import EditSVG from "./components/EditSVG"
-import Controls from "./components/Controls"
+import Home from "./pages/Home"
+import {Signup} from "./pages/Signup"
+import { Login } from "./pages/Login"
+import { AuthModal } from "./components/AuthModal"
 
 function App() {
-  const [svgList, setSvgList] = useState([])
+  const [svgList, setSvgList] = useState(
+    (localStorage["svgList"] && localStorage["svgList"] !== "undefined") ? 
+    JSON.parse(localStorage["svgList"]) : []
+  )
   const [isEditing, setIsEditing] = useState(false)
   const [editingIdx, setEditingIdx] = useState()
-  const [editSvg, setEditSvg] = useState({})
+  const [editSvg, setEditSvg] = useState([])
+  const [token, setToken] = useState(
+    localStorage["token"] !== "undefined" ? 
+    localStorage["token"] : undefined
+  )
+  const [tokenExpiration, setTokenExpiration] = useState(
+    localStorage["tokenExpiration"] !== "undefined" ? 
+    localStorage["tokenExpiration"] : undefined
+  )
+  const [boundaries, setBoundaries] = useState(
+    (localStorage["boundaries"] && localStorage["boundaries"] !== "undefined") ?
+    JSON.parse(localStorage["boundaries"]) : []
+  )
+  const [isDeleting, setIsDeleting] = useState(false)
+  const [globalPoints, setGlobalPoints] = useState({
+    points: [],
+    adding: false
+  })
 
-  const handleClick =(e) => {
-    setEditSvg(prevEditSvg => {
-      const newEditSvg = JSON.parse(JSON.stringify(prevEditSvg))
-      if(!newEditSvg.vectors?.length)newEditSvg.vectors = []
-      newEditSvg.vectors.push({
-        lat: e.x,
-        long: e.y
-      })
-
-      return newEditSvg
-    })
-  }
   useEffect(() => {
-    if(isEditing){
-      document.addEventListener("click", handleClick)
-    }
     if(!isEditing){
-      if(editSvg?.vectors?.length)setSvgList(prevSvgList => [...prevSvgList, editSvg])
+      if(editSvg.length > 2)setSvgList(prevSvgList => [...prevSvgList, editSvg])
     }
     return () => {
-      document.removeEventListener("click", handleClick)
-      setEditSvg({})
+      setEditSvg([])
     }
   }, [isEditing])
+
+  useEffect(() => {
+    localStorage["svgList"] = svgList
+    if(svgList?.length && JSON.stringify(svgList) !== JSON.stringify(boundaries)){
+      const body = JSON.stringify({
+        boundaries: svgList
+      })
+      const requestOptions = {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${token}`
+        },
+        body: body
+      };
+    
+      fetch(import.meta.env.VITE_REACT_SERVER_URI + "/user", requestOptions)
+      .then(res => res.text())
+      .then(res => {
+        console.log(res)
+        setBoundaries(svgList)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+
+  }, [svgList])
+
+  useEffect(() => {
+    localStorage["token"] = token
+    localStorage["tokenExpiration"] = tokenExpiration
+    localStorage["boundaries"] = JSON.stringify(boundaries)
+    localStorage["svgList"] = JSON.stringify(svgList)
+  }, [token, tokenExpiration, svgList, boundaries])
+
+  const logout = () => {
+    setToken()
+    setTokenExpiration()
+  }
+
+  useEffect(() => {
+    if(!tokenExpiration)return
+    const tokenTimeout = setTimeout(logout, new Date(tokenExpiration) - new Date())
+    return () => {
+      clearTimeout(tokenTimeout)
+    }
+  }, [tokenExpiration])
+
   return (
-    <AppContext.Provider
-      value={{
-        isEditing,
-        editingIdx,
-        editSvg,
-        svgList,
-        setSvgList,
-        setEditingIdx,
-        setIsEditing,
-        setEditSvg
-      }}
-    >
-      <Container>
-        <div className="boundary-container">
-          <svg className={isEditing ? "editing": ""}>
-            {
-              svgList.map((svg, key) => <SVG key={key} {...svg} />)
-            }
-            {
-              isEditing && <EditSVG {...editSvg} />
-            }
-          </svg>
-        </div>
-        <Controls />
-      </Container>
-    </AppContext.Provider>
+    <Container>
+      <AppContext.Provider
+        value={{
+          isEditing,
+          editingIdx,
+          editSvg,
+          svgList,
+          setSvgList,
+          setEditingIdx,
+          setIsEditing,
+          setEditSvg,
+          token,
+          setToken,
+          tokenExpiration,
+          setTokenExpiration,
+          boundaries,
+          setBoundaries,
+          isDeleting,
+          setIsDeleting,
+          globalPoints,
+          setGlobalPoints
+        }}
+      >
+        {!token?.length && <AuthModal />}
+      </AppContext.Provider>
+    </Container>
   )
 }
 
