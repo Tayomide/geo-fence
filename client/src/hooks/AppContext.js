ecc76bbc136dc738a4d3101b6d6ef3f9b423265e
Tayomide <70061548+Tayomide@users.noreply.github.com>
2023-11-04 12:02:15 -0500

    Remove unnecessary global variables

--- client/src/hooks/AppContext.js
+++ client/src/hooks/AppContext.js
@@ -1,28 +1,13 @@
 import { createContext } from "react";
 
 const AppContext = createContext({
-  isEditing: false,
-  setIsEditing: () => {},
-  editingIdx: -1,
-  setEditingIdx: () => {},
-  editSvg: [],
-  setEditSvg: () => {},
-  svgList: [],
-  setSvgList: () => {},
   // Authentication
   token: null,
   setToken: () => {},
   tokenExpiration: null,
   setTokenExpiration: () => {},
-  boundaries: [],
-  setBoundaries: () => {},
-  isDeleting: false,
-  setIsDeleting: () => {},
-  globalPoints: {
-    points: [],
-    adding: false
-  },
-  setGlobalPoints: () => {}
+  loggedIn: false,
+  setLoggedIn: () => {}
 })
 
 export default AppContext
